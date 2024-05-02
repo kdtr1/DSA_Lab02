229df893142589f806203ac5ebfd7432727a8ea7
kdtr1 <139198743+kdtr1@users.noreply.github.com>
2024-05-02 20:38:39 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,106 @@
+#include "algorithm_design.h"
+#include "operation_counting.h"
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+void print_line(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "-";
+    }
+    cout << endl;
+}
+void copy_array(int* a, int* sub, int n) {
+    for (int i = 0; i < n; i++) {
+        sub[i] = a[i];
+    }
+}
+
+int main() {
+    // 1. Operation counting (Assignments and Comparisons):
+    cout << "1. Operation counting (Assignments and Comparisons):" << endl << endl;
+    // (i) Sum half:
+    cout << "(i) Sum half:" << endl;
+    cout << setw(10) << "n" << setw(15) << "Assignments" << setw(15) << "Comparisons" << endl;
+    for (int n = 0; n <= 500; n += 25) {
+        int assign_count_1 = 0, compare_count_1 = 0;
+        sum_half(n, assign_count_1, compare_count_1);
+        cout << setw(10) << n << setw(15) << assign_count_1 << setw(15) << compare_count_1 << endl;
+    }
+    cout << endl;  print_line(100); cout << endl;
+
+    // (ii) Recursive square sum:
+    cout << "(ii) Recursive square sum:" << endl;
+    cout << setw(10) << "n" << setw(15) << "Comparisons" << endl;
+    for (int n = 0; n <= 500; n += 25) {
+        int compare_count_2 = 0;
+        recursive_square_sum(n, compare_count_2);
+        cout << setw(10) << n << setw(15) << compare_count_2 << endl;
+    }
+    cout << endl;  print_line(100); cout << endl;
+
+
+    // 2. Algorithm Design:
+    cout << "2. Algorithm Design:" << endl << endl;
+    // (i) GCD:
+    int u = 48, v = 18;
+    int count_assign_A_1 = 0, count_compare_A_1 = 0;
+    int count_assign_A_2 = 0, count_compare_A_2 = 0;
+    cout << "(i) Greatest common divisor:" << endl;
+    cout << setw(45) << "Result" << setw(17) << "Assignments" << setw(15) << "Comparisons" << endl;
+    int res_A_1 = GCD_simple(u, v, count_assign_A_1, count_compare_A_1);
+    int res_A_2 = GCD_Euclid(u, v, count_assign_A_2, count_compare_A_2);
+    cout << "Algorithm 1 (Simple Division):" << setw(13) << res_A_1 << setw(14) << count_assign_A_1 << setw(15) << count_compare_A_1 << endl;
+    cout << "Algorithm 2 (Euclid's Algorithm):" << setw(10) << res_A_2 << setw(14) << count_assign_A_2 << setw(15) << count_compare_A_2 << endl;
+    cout << endl;  print_line(100); cout << endl;
+
+
+    // (ii) LIS:
+    int a_B[] = { 1, 2, 3, 2, 4, 5, 6, 3, 7 };
+    int n_B = sizeof(a_B) / sizeof(a_B[0]);
+    int count_assign_B_1 = 0, count_compare_B_1 = 0;
+    int count_assign_B_2 = 0, count_compare_B_2 = 0;
+    cout << "(ii) Longest increasing subsequence:" << endl;
+    cout << setw(45) << "Result" << setw(17) << "Assignments" << setw(15) << "Comparisons" << endl;
+    int res_B_1 = LIS_simple(a_B, n_B, count_assign_B_1, count_compare_B_1);
+    int res_B_2 = LIS_Brute_Force(a_B, n_B, count_assign_B_2, count_compare_B_2);
+    cout << "Algorithm 1 (Simple LIS):" << setw(18) << res_B_1 << setw(14) << count_assign_B_1 << setw(15) << count_compare_B_1 << endl;
+    cout << "Algorithm 2 (Brute-Force Approach):" << setw(8) << res_B_2 << setw(14) << count_assign_B_2 << setw(15) << count_compare_B_2 << endl;
+    cout << endl;  print_line(100); cout << endl;
+
+
+    // (iii) Median of array:
+    int a_C[] = { 1, 20, 6, 4, 5 };
+    int n_C = sizeof(a_C) / sizeof(a_C[0]);
+    int* sub_C_1 = new int[n_C]; copy_array(a_C, sub_C_1, n_C);
+    int* sub_C_2 = new int[n_C]; copy_array(a_C, sub_C_2, n_C);
+    int* B_1 = new int[n_C]; int* B_2 = new int[n_C];
+    int count_assign_C_1 = 0, count_compare_C_1 = 0;
+    int count_assign_C_2 = 0, count_compare_C_2 = 0;
+    cout << "(iii) Median of array:" << endl;
+    cout << setw(53) << "Result" << setw(17) << "Assignments" << setw(15) << "Comparisons" << endl;
+    calculate_median_Brute_Force(sub_C_1, n_C, B_1, count_assign_C_1, count_compare_C_1);
+    calculate_median_Heap(sub_C_2, n_C, B_2, count_assign_C_2, count_compare_C_2);
+    cout << "Algorithm 1 (Brute Force Algorithm):" << setw(9); print_median(B_1, n_C); cout << setw(10) << count_assign_C_1 << setw(15) << count_compare_C_1 << endl;
+    cout << "Algorithm 2 (Heap structure Algorithm):" << setw(6); print_median(B_2, n_C); cout << setw(10) << count_assign_C_2 << setw(15) << count_compare_C_2 << endl;
+    delete[] B_1, B_2, sub_C_1, sub_C_2;
+    cout << endl;  print_line(100); cout << endl;
+
+    //(iv) Count inversions:
+    int a_D[] = { 1, 3, 5, 4, 2 };
+    int n_D = sizeof(a_D) / sizeof(a_D[0]);
+    int* sub_D_1 = new int[n_C]; copy_array(a_D, sub_D_1, n_D);
+    int* sub_D_2 = new int[n_C]; copy_array(a_D, sub_D_2, n_D);
+    int count_assign_D_1 = 0, count_compare_D_1 = 0;
+    int count_assign_D_2 = 0, count_compare_D_2 = 0;
+    cout << "(iv) Count inversions:" << endl;
+    cout << setw(53) << "Result" << setw(17) << "Assignments" << setw(15) << "Comparisons" << endl;
+    int res_D_1 = count_inversions_Bruce_Force(sub_D_1, n_D, count_assign_D_1, count_compare_D_1);
+    int res_D_2 = count_inversions_Divide_and_Conquer(sub_D_2, n_D, count_assign_D_2, count_compare_D_2);
+    cout << "Algorithm 1 (Brute Force Algorithm):" << setw(15) << res_D_1 << setw(14) << count_assign_D_1 << setw(15) << count_compare_D_1 << endl;
+    cout << "Algorithm 2 (Divide and Conquer Algorithm):" << setw(8) << res_D_2 << setw(14) << count_assign_D_2 << setw(15) << count_compare_D_2 << endl;
+    cout << endl;  print_line(100); cout << endl;
+
+    return 0;
+}
