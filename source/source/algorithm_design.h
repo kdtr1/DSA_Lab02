c4992bc2ee0c29436dc35e1b58e85b5bf908e49d
HeyKayDi <153514122+HeyKayDi@users.noreply.github.com>
2024-05-04 11:40:04 +0700

    Update algorithm_design.h

--- source/source/algorithm_design.h
+++ source/source/algorithm_design.h
@@ -15,7 +15,7 @@ void swap(int& a, int& b) {
 
 // 2. Algorithm Design:
 // (i) Greatest common divisor: (GCD)
-// Algorithm 1 (Bruce-Force):
+// Algorithm 1 (Bruce-Force Method):
 int GCD_Bruce_Force(int a, int b, long long& count_assign, long long& count_compare) {
     int gcd = 1;
     count_assign++; // Assign gcd = 1
