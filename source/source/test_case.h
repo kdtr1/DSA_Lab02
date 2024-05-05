8a3ca7155a50901f5b31285f33900b6e8ea16c5b
HeyKayDi <153514122+HeyKayDi@users.noreply.github.com>
2024-05-05 17:50:34 +0700

    Update test_case.h

--- source/source/test_case.h
+++ source/source/test_case.h
@@ -1,78 +1,89 @@
-﻿#pragma once
+#pragma once
+#ifndef _TEST_CASE_H_
+#define _TEST_CASE_H_
 #include <cmath>
 #include <time.h>
+#include <random>
+#include <algorithm>
 using namespace std;
 
 template <class T>
 void swap(T& a, T& b) {
-	T x = a;
-	a = b;
-	b = x;
+    T x = a;
+    a = b;
+    b = x;
 }
 void print(int* a, int n) {
-	for (int i = 0; i < n; i++) {
-		cout << a[i] << " ";
-	}
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
 }
 
 //Generate random data number:
 void generate_random_data_number(int& a, int& b) {
-	srand((unsigned int)time(nullptr));
-	a = rand() % 100;
-	b = rand() % 100;
+    srand((unsigned int)time(nullptr));
+    a = rand() % 100;
+    b = rand() % 100;
 }
 
 // Generate random array of data:
-void generate_random_data(int* a, int n) {
-	srand((unsigned int)time(nullptr));
-
-	for (int i = 0; i < n; i++) {
-		a[i] = rand() % n;
-	}
+void generate_random_data(int*& a, int n) {
+    a = new int[n];
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 100);
+    for (int i = 0; i < n; i++) {
+        a[i] = dis(gen);
+    }
 }
 
 // Generate data array in ascending order:
-void generate_sorted_data(int* a, int n) {
-	for (int i = 0; i < n; i++) {
-		a[i] = i;
-	}
+int* generate_sorted_data(int* a, int n) {
+    int* res = new int[n];
+    for (int i = 0; i < n; i++) {
+        res[i] = a[i];
+    }
+    sort(res, res + n);
+    return res;
 }
 
 // Generate data array in reverse order (descending):
-void generate_reverse_data(int* a, int n) {
-	for (int i = 0; i < n; i++) {
-		a[i] = n - 1 - i;
-	}
+int* generate_reverse_data(int* a, int n) {
+    int* res = new int[n];
+    for (int i = 0; i < n; i++) {
+        res[i] = a[i];
+    }
+    sort(res, res + n, greater<int>());
+    return res;
 }
 
-// Generates roughly ordered data array:
-void generate_nearly_sorted_data(int* a, int n) {
-	for (int i = 0; i < n; i++) {
-		a[i] = i;
-	}
-	srand((unsigned int)time(nullptr));
-	for (int i = 0; i < 10; i++) {
-		int r1 = rand() % n;
-		int r2 = rand() % n;
-		swap(a[r1], a[r2]);
-	}
+void generate_data(int*& a, int n, int data_type) {
+    switch (data_type) {
+    case 0: // random
+        generate_random_data(a, n);
+        cout << "Array after generating random data:" << endl;
+        print(a, n);
+        break;
+    case 1: { // random ascending
+        int* temp_1 = nullptr;
+        generate_random_data(temp_1, n);
+        int* ans = generate_sorted_data(temp_1, n);
+        cout << "Array after generating random data:" << endl;
+        print(ans, n);
+        break;
+    }
+    case 2: { // random descending
+        int* temp_2 = nullptr;
+        generate_random_data(temp_2, n);
+        int* res = generate_reverse_data(temp_2, n);
+        cout << "Array after generating random data:" << endl;
+        print(res, n);
+        break;
+    }
+    default:
+        cout << "Error: unknown data type!" << endl;
+    }
 }
 
-void generate_data(int* a, int n, int data_type) {
-	switch (data_type) {
-	case 0:	// ngẫu nhiên
-		generate_random_data(a, n);
-		break;
-	case 1:	// có thứ tự
-		generate_sorted_data(a, n);
-		break;
-	case 2:	// có thứ tự ngược
-		generate_reverse_data(a, n);
-		break;
-	case 3:	// gần như có thứ tự
-		generate_nearly_sorted_data(a, n);
-		break;
-	default:
-		cout << "Error: unknown data type!" << endl;
-	}
-}
+#endif 
